3028b8b6dcc487befeeebe5bc4d712db4fb5fe06
ahmadrahmat14 <ahmadrahmat14@gmail.com>
2023-10-19 13:14:30 +0800

    22 Get Position

--- lib/app/controllers/page_index_controller.dart
+++ lib/app/controllers/page_index_controller.dart
@@ -1,4 +1,5 @@
 import 'package:get/get.dart';
+import 'package:geolocator/geolocator.dart';
 import 'package:presence/app/routes/app_pages.dart';
 
 class PageIndexController extends GetxController {
@@ -9,7 +10,16 @@ class PageIndexController extends GetxController {
     switch (i) {
       case 1:
         // Get.offAllNamed(Routes.ADD);
-        print("ABSENSI");
+        // print("ABSENSI");
+        Map<String, dynamic> dataResponse = await determinePosition();
+        if (dataResponse['error'] != true) {
+          Position position = dataResponse['position'];
+          // print("LATITUDE: ${position.latitude}, LONGITUDE: ${position.longitude}");
+          Get.snackbar(dataResponse['message'],
+              "LATITUDE: ${position.latitude}, LONGITUDE: ${position.longitude}");
+        } else {
+          Get.snackbar("Terjadi kesalahan", dataResponse['message']);
+        }
         break;
       case 2:
         pageIndex.value = i;
@@ -19,5 +29,59 @@ class PageIndexController extends GetxController {
         pageIndex.value = i;
         Get.offAllNamed(Routes.HOME);
     }
+  }
+
+  Future<Map<String, dynamic>> determinePosition() async {
+    bool serviceEnabled;
+    LocationPermission permission;
+
+    // Test if location services are enabled.
+    serviceEnabled = await Geolocator.isLocationServiceEnabled();
+    if (!serviceEnabled) {
+      // Location services are not enabled don't continue
+      // accessing the position and request users of the
+      // App to enable the location services.
+      return {
+        "message": "Layanan lokasi dinonaktifkan.",
+        "error": true,
+      };
+      // return Future.error('Location services are disabled.');
+    }
+
+    permission = await Geolocator.checkPermission();
+    if (permission == LocationPermission.denied) {
+      permission = await Geolocator.requestPermission();
+      if (permission == LocationPermission.denied) {
+        // Permissions are denied, next time you could try
+        // requesting permissions again (this is also where
+        // Android's shouldShowRequestPermissionRationale
+        // returned true. According to Android guidelines
+        // your App should show an explanatory UI now.
+        return {
+          "message": "Izin lokasi ditolak.",
+          "error": true,
+        };
+        // return Future.error('Location permissions are denied');
+      }
+    }
+
+    if (permission == LocationPermission.deniedForever) {
+      // Permissions are denied forever, handle appropriately.
+      return {
+        "message":
+            "Izin lokasi ditolak secara permanen, kami tidak dapat meminta izin.",
+        "error": true,
+      };
+      // return Future.error('Location permissions are permanently denied, we cannot request permissions.');
+    }
+
+    // When we reach here, permissions are granted and we can
+    // continue accessing the position of the device.
+    Position position = await Geolocator.getCurrentPosition();
+    return {
+      "position": position,
+      "message": "Berhasil mendapatkan posisi device.",
+      "error": false,
+    };
   }
 }
